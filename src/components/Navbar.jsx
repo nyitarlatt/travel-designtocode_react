ca23430025f7df556ae356017b3e6fa47cbd0c85
nyitarlatt <nyitarlat@gmail.com>
2022-05-27 00:32:14 +0630

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import MenuIcon from "@mui/icons-material/Menu";
 
 import { Logo } from "../assets";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState();
+  const [showMenuBtn, setShowMenuBtn] = useState(true);
+
+  const displayMenu = () => {};
+
   return (
     <div>
       <div className="w-4/5 mx-auto flex justify-between py-8 relative">
@@ -33,7 +40,58 @@ const Navbar = () => {
             <div className="hover:text-travelBlue">En</div>
           </div>
         </div>
-        <div className="absolute top-10 right-0 w-1/3 bg-travelBlueLight"></div>
+        <div className="flex items-center justify-center lg:hidden">
+          <MenuIcon
+            onClick={() => {
+              setShowMenu(true);
+            }}
+          />
+        </div>
+        {showMenu && (
+          <div className="absolute top-10 right-0 w-1/4 bg-white text-sm text-black flex flex-col px-2 border-2">
+            <div
+              className="text-2xl ml-auto pr-2 cursor-pointer"
+              onClick={() => {
+                setShowMenu(false);
+              }}
+            >
+              x
+            </div>
+            <a
+              href="#"
+              className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black"
+            >
+              Destinations
+            </a>
+            <a
+              href="#"
+              className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black"
+            >
+              Hotels
+            </a>
+            <a
+              href="#"
+              className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black"
+            >
+              Flights
+            </a>
+            <a
+              href="#"
+              className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black"
+            >
+              Bookings
+            </a>
+            <div className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black">
+              Login
+            </div>
+            <div className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black">
+              Sign Up
+            </div>
+            <div className="hover:bg-travelBlack hover:text-white  w-full py-2 border-b-2 border-black">
+              En
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
