7c5944588ceeff59c0a650a8195dac4c1bb1cff3
nyitarlatt <nyitarlat@gmail.com>
2022-06-06 22:48:06 +0630

    home

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,3 @@
 export { default as Logo } from "./Logo.png";
 export { default as image } from "./image.png";
+export { default as polygon } from "./polygon.png";
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-import { image } from "../assets";
+import { image, polygon } from "../assets";
 
 const Home = () => {
   return (
-    <div id="home bg-black">
-      <div className="w-4/5 mx-auto w-4/5  flex flex-col lg:flex-row">
-        <div className="flex-1 flex flex-col space-y-10 mt-20">
-          <div classname="font-poppins text-sm bg-black">
-            Best Destinations around the worlds
+    <div id="home">
+      <div className="w-4/5 mx-auto flex flex-col lg:flex-row">
+        <div className="flex-1 flex flex-col space-y-6 mt-20 items-center lg:items-start">
+          <div className="uppercase text-travelRed font-bold">
+            Best Destinations around the world
           </div>
-          <h1>Travel, enjoy and live a new and full life</h1>
-          <p classname="font-poppins bg-black">
+          <div className="text-black text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter">
+            Travel, enjoy <br />
+            and live a new <br />
+            and full life
+          </div>
+          <div className="text-base text-travelTextLight">
             Built Wicket longer admire do barton vanity itself do in it.
             Preferred to sportsmen it engrossed listening. Park gate sell they
-            west hard for the
-          </p>
-          <div className="flex space-x-10">
-            <div>aasdfsadfsadfsdfsd</div>
-            <div>a</div>
+            west hard for the.
+          </div>
+          <div className="flex space-x-8">
+            <div className="bg-travelYellow py-3 px-4 rounded-xl text-white">
+              Find out more
+            </div>
+            <div className="flex justify-between items-center space-x-4">
+              <div className="flex items-center justify-center bg-travelRed w-12 h-12 rounded-full">
+                <img src={polygon} alt="" />
+              </div>
+              <div className="text-travelTextLight text-lg">Play Demo</div>
+            </div>
           </div>
         </div>
         <div className="flex-1">
-          <img src={image} alt="" />
+          <img src={image} alt="travel" />
         </div>
       </div>
     </div>
